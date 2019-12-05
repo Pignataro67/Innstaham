1df44a27436106b7345f46d99351c1a776027a77
Pignataro67 <Pignataro67@gmail.com>
2019-12-05 12:28:50 -0800

    some app cont. crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+  before_action :set_previous_url
+  helper_method :previous_url
+
+    def set_previous_url
+      if request.method == :get && session[:previous_url] != session[:current_url]
+        session[:previous_url] == session[:current_url]
+        session[:current_url] = request.url
+      end
+    end
+
+    def previous_url
+      @last_page = session[:previous_url]
+    end
+
+
+    def index
+    end
+
+
+    def current_user
+      if session[:user_id] == nil
+        # redirect_to users_path
+        return nil
+      else
+      @current_user = User.find(session[:user_id])
+      end
+    end
+
+    def authorization?
+      current_user.id == params[:id].to_i
+    end
+  end
 end
