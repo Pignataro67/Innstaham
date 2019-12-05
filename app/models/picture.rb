4271f25e65b86e1c6a4aa2dfd8f14fa5f7617b41
Pignataro67 <Pignataro67@gmail.com>
2019-12-05 12:41:54 -0800

    pic model, view

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,9 @@
 class Picture < ApplicationRecord
+  belongs_to :user
+  has_many :likes
+  has_many :icons, through: :likes
+  has_many :comments
+  has_one_attached :img_url
+  has_many :picture_hashtags
+  has_many :hashtags, through: :picture_hashtags
 end
