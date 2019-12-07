daeede0df18f1093fac92601e0d52bd4fb52378a
Pignataro67 <Pignataro67@gmail.com>
2019-12-07 05:58:52 -0800

    adding like model and cont.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,33 @@
 class LikesController < ApplicationController
+  before_action :set_info, only: [:edit, :update, :destroy, :create]
+
+  def new
+    @like = Like.new
+  end
+  #gravita icons gem
+  def create
+    @like = Like.new(icon_id: params[:icon_id], user_id: current_user.id, picture_id: params[:picture_id])
+  
+    if @like.save
+      redirect_to "/users/#{@user.id}/pictures/#{@picture.id}"
+    else
+      render :new
+    end
+  end
+    
+  def edit  
+  end
+  
+  def update  
+  end
+  
+  def destroy  
+  end
+  
+  private
+  
+  def set_info
+    @user = User.find(params[:user_id])
+    @picture = Picture.find(params[:picture_id])
+  end
 end
