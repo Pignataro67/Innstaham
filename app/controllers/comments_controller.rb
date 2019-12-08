3a62e1199adf303772729dd7ae6f9377fb85d3c4
Pignataro67 <Pignataro67@gmail.com>
2019-12-08 12:44:35 -0800

    more crud in com. cont.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,32 @@
 class CommentsController < ApplicationController
   before_action :set_info, only: [:edit, :update, :destroy, :create]
   
+  def new
+    @comment = Comment.new
+  end
+  
+  def create
+
+    @comment = Comment.new(content: params[:comment][:content], user_id: current_user.id, picture_id: params[:picture_id])
+    if @comment.save
+      hashtag_check(@comment.content)
+      find_or_create_by
+      # redirect_to user_picture_path(@picture)
+      redirect_to "/users/#{@user.id}/pictures/#{@picture.id}"
+    else
+      redirect_to "/users/#{@user.id}/pictures/#{@picture.id}"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
   def set_info
     @user = User.find(params[:user_id])
--- app/models/picture_hashtag.rb
+++ app/models/picture_hashtag.rb
@@ -1,2 +1,4 @@
 class PictureHashtag < ApplicationRecord
+  belongs_to :picture
+  belongs_to :hashtag
 end
