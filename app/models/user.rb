2f7eb0a1a31e0df1ca7cb5bd4281e407c967f3bd
Pignataro67 <Pignataro67@gmail.com>
2019-12-13 06:03:21 -0800

    adding method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   def authenticate(plaintext_password)
     BCrypt::Password.new(self.password_digest) == plaintext_password
   end
+
+  def set_img_url
+    self.img_url.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'snowflake.png')), filename: 'snowflake.png', content_type: 'image/png')	    self.img_url.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'snowflake.png')), filename: 'snowflake.png', content_type: 'image/png')
+  end	 
 end
