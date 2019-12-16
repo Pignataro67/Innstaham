455d1fa46d41c0c8c83acdb03a050f69910a1670
Pignataro67 <Pignataro67@gmail.com>
2019-12-16 12:07:47 -0800

    trying to get bcrypt involved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
-# require 'bcrypt'
+require 'bcrypt'
 class User < ApplicationRecord
   validates_confirmation_of :password
-  # has_secure_password
+  has_secure_password
   
   def authenticate(plaintext_password)
     BCrypt::Password.new(self.password_digest) == plaintext_password
   end
 
-  # def set_img_url
-  #   self.img_url.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'snowflake.png')), filename: 'snowflake.png', content_type: 'image/png')	    self.img_url.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'snowflake.png')), filename: 'snowflake.png', content_type: 'image/png')
-  # end	 
+  def set_img_url
+    self.img_url.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'snowflake.png')), filename: 'snowflake.png', content_type: 'image/png')	    self.img_url.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'snowflake.png')), filename: 'snowflake.png', content_type: 'image/png')
+  end	 
 end
